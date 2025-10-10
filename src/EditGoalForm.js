09ab4677bf3d83a7e3ae93d1fd0004ac3a18349d
Salina Ortega <sortega2@illinois.edu>
2025-10-09 23:08:20 -0700

    add edit form

--- src/EditGoalForm.js
+++ src/EditGoalForm.js
@@ -1,45 +1,21 @@
 import React, { useState } from 'react';
 import {
   Dialog, DialogActions, DialogContent, DialogTitle,
-  TextField, Button, Box, Autocomplete, Fab
+  TextField, Button, Box, Autocomplete, IconButton
 } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const EditGoalForm = ({ goal, open, handleClose, editGoal, deleteGoal, categories, goals}) => {
-//   const [goalDetails, setGoalDetails] = useState({
-//     label: '',
-//     category: '', // User can type or select this value
-//     dueDate: '',
-//   });
+  const [goalDetails, setGoalDetails] = useState({
+    ...goal
+  });
 
-//   const handleChange = (field, value) => {
-//     setGoalDetails(prev => ({ ...prev, [field]: value }));
-//   };
-
-  //const [open, setOpen] = useState(false);
-
-//   const handleSubmit = () => {
-//     console.log('Form submitted with:', goalDetails);
-//     if (goalDetails.label && goalDetails.dueDate && goalDetails.category) {
-//       const goalToAdd = {
-//         ...goalDetails,
-//         parentId: null,
-//         status: 'incomplete'
-//       };
-//       console.log('Calling addGoal with:', goalToAdd);
-//       addGoal(goalToAdd);
-//       setOpen(false);
-//       setGoalDetails({ label: '', category: '', dueDate: '' }); // Reset form
-//     } else {
-//       console.log('Form validation failed - missing fields');
-//     }
-//   };
-
-//   const handleClose = () => setOpen(false);
+  const handleChange = (field, value) => {
+    setGoalDetails(prev => ({ ...prev, [field]: value }));
+  };
 
   return (
     <>
-      
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Edit Goal</DialogTitle>
         <DialogContent>
@@ -47,12 +23,12 @@ const EditGoalForm = ({ goal, open, handleClose, editGoal, deleteGoal, categorie
             {/* Goal Name */}
             <TextField
               label="Goal Name"
-              value={goal.label}
-              //onChange={(e) => handleChange('label', e.target.value)}
+              value={goalDetails.label}
+              onChange={(e) => handleChange('label', e.target.value)}
               fullWidth
             />
 
-            {/* Editable Category Field
+            {/* Editable Category Field */}
             <Autocomplete
               options={categories} // Existing categories
               freeSolo // Allows custom input
@@ -67,23 +43,30 @@ const EditGoalForm = ({ goal, open, handleClose, editGoal, deleteGoal, categorie
                   fullWidth
                 />
               )}
-            /> */}
+            />
 
             {/* Due Date */}
-            {/* <TextField
+            <TextField
               label="Due Date"
               type="date"
               value={goalDetails.dueDate}
               onChange={(e) => handleChange('dueDate', e.target.value)}
               InputLabelProps={{ shrink: true }}
               fullWidth
-            /> */}
+            />
           </Box>
         </DialogContent>
-        {/* <DialogActions>
-          <Button onClick={handleClose} color="secondary">Cancel</Button>
-          <Button onClick={handleSubmit} color="primary">Save</Button>
-        </DialogActions> */}
+        <DialogActions>
+            <IconButton
+                onClick={() => deleteGoal(goalDetails.id)}
+                aria-label="filter settings"
+                sx={{ color: 'white' }}
+            >
+                <DeleteIcon />
+            </IconButton>
+            <Button onClick={handleClose} color="secondary">Cancel</Button>
+            <Button onClick={() => editGoal(goalDetails.id, { label: goalDetails.label, category: goalDetails.category, dueDate: goalDetails.dueDate })} color="primary">Save</Button>
+        </DialogActions>
       </Dialog>
     </>
   );
